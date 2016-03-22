542c9b9eff6db4da40d184eebfb60af74696e961
Dan Walsh <dwalsh@redhat.com>
2016-03-22 13:19:40 -0400

    Add RHEL super secrets patch
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- daemon/oci_linux.go
+++ daemon/oci_linux.go
@@ -712,6 +712,16 @@ func (daemon *Daemon) createSpec(c *container.Container) (*specs.Spec, error) {
 		ms = append(ms, *m)
 	}
 
+	rootUID, rootGID := daemon.GetRemappedUIDGID()
+	m, err := c.SecretMountRHEL(rootUID, rootGID)
+	if err != nil {
+		return nil, err
+	}
+	// SecretMountRHEL() returns m == nil && err == nil
+	// we check m before appending and dereferencing it
+	if m != nil {
+		ms = append(ms, *m)
+	}
 	sort.Sort(mounts(ms))
 	if err := setMounts(daemon, &s, c, ms); err != nil {
 		return nil, fmt.Errorf("linux mounts: %v", err)
