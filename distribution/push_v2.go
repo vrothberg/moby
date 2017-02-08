slcmpunk/docker

Allow to skip manifest schema V2 push
Signed-off-by: Antonio Murdaca <runcom@redhat.com>

distribution/push_v2.go
@@ -155,30 +155,42 @@ func (p *v2Pusher) pushV2Tag(ctx context.Context, ref reference.NamedTagged, ima
 		return err
 	}
 
-	// Try schema2 first
-	builder := schema2.NewManifestBuilder(p.repo.Blobs(ctx), img.RawJSON())
-	manifest, err := manifestFromBuilder(ctx, builder, descriptors)
-	if err != nil {
-		return err
-	}
-
 	manSvc, err := p.repo.Manifests(ctx)
 	if err != nil {
 		return err
 	}
 
-	putOptions := []distribution.ManifestServiceOption{client.WithTag(ref.Tag())}
-	if _, err = manSvc.Put(ctx, manifest, putOptions...); err != nil {
-		logrus.Warnf("failed to upload schema2 manifest: %v - falling back to schema1", err)
-
-		builder = schema1.NewConfigManifestBuilder(p.repo.Blobs(ctx), p.config.TrustKey, p.repo.Name(), ref.Tag(), img.RawJSON())
+	var (
+		manifest   distribution.Manifest
+		putOptions = []distribution.ManifestServiceOption{client.WithTag(ref.Tag())}
+	)
+	if p.config.SkipSchemaV2 {
+		builder := schema1.NewConfigManifestBuilder(p.repo.Blobs(ctx), p.config.TrustKey, p.repo.Name(), ref.Tag(), img.RawJSON())
 		manifest, err = manifestFromBuilder(ctx, builder, descriptors)
 		if err != nil {
 			return err
 		}
-
 		if _, err = manSvc.Put(ctx, manifest, putOptions...); err != nil {
 			return err
+		}
+	} else {
+		// Try schema2 first
+		builder := schema2.NewManifestBuilder(p.repo.Blobs(ctx), img.RawJSON())
+		manifest, err = manifestFromBuilder(ctx, builder, descriptors)
+		if err != nil {
+			return err
+		}
+		if _, err = manSvc.Put(ctx, manifest, putOptions...); err != nil {
+			logrus.Warnf("failed to upload schema2 manifest: %v - falling back to schema1", err)
+
+			builder = schema1.NewConfigManifestBuilder(p.repo.Blobs(ctx), p.config.TrustKey, p.repo.Name(), ref.Tag(), img.RawJSON())
+			manifest, err = manifestFromBuilder(ctx, builder, descriptors)
+			if err != nil {
+				return err
+			}
+			if _, err = manSvc.Put(ctx, manifest, putOptions...); err != nil {
+				return err
+			}
 		}
 	}
 

vrothberg/moby

distribution/push_v2.go
@@ -174,39 +174,54 @@ func (p *v2Pusher) pushV2Tag(ctx context.Context, ref reference.NamedTagged, id 
 		return err
 	}
 
-	// Try schema2 first
-	builder := schema2.NewManifestBuilder(p.repo.Blobs(ctx), p.config.ConfigMediaType, imgConfig)
-	manifest, err := manifestFromBuilder(ctx, builder, descriptors)
-	if err != nil {
-		return err
-	}
-
 	manSvc, err := p.repo.Manifests(ctx)
 	if err != nil {
 		return err
 	}
 
-	putOptions := []distribution.ManifestServiceOption{distribution.WithTag(ref.Tag())}
-	if _, err = manSvc.Put(ctx, manifest, putOptions...); err != nil {
-		if runtime.GOOS == "windows" || p.config.TrustKey == nil || p.config.RequireSchema2 {
-			logrus.Warnf("failed to upload schema2 manifest: %v", err)
-			return err
-		}
-
-		logrus.Warnf("failed to upload schema2 manifest: %v - falling back to schema1", err)
-
+	var (
+		manifest   distribution.Manifest
+		putOptions = []distribution.ManifestServiceOption{distribution.WithTag(ref.Tag())}
+	)
+	if p.config.SkipSchemaV2 {
 		manifestRef, err := distreference.WithTag(p.repo.Named(), ref.Tag())
 		if err != nil {
 			return err
 		}
-		builder = schema1.NewConfigManifestBuilder(p.repo.Blobs(ctx), p.config.TrustKey, manifestRef, imgConfig)
+		builder := schema1.NewConfigManifestBuilder(p.repo.Blobs(ctx), p.config.TrustKey, manifestRef, imgConfig)
 		manifest, err = manifestFromBuilder(ctx, builder, descriptors)
 		if err != nil {
 			return err
 		}
-
 		if _, err = manSvc.Put(ctx, manifest, putOptions...); err != nil {
 			return err
+		}
+	} else {
+		builder := schema2.NewManifestBuilder(p.repo.Blobs(ctx), p.config.ConfigMediaType, imgConfig)
+		manifest, err = manifestFromBuilder(ctx, builder, descriptors)
+		if err != nil {
+			return err
+		}
+		if _, err = manSvc.Put(ctx, manifest, putOptions...); err != nil {
+			if runtime.GOOS == "windows" {
+				logrus.Warnf("failed to upload schema2 manifest: %v", err)
+				return err
+			}
+
+			logrus.Warnf("failed to upload schema2 manifest: %v - falling back to schema1", err)
+
+			manifestRef, err := distreference.WithTag(p.repo.Named(), ref.Tag())
+			if err != nil {
+				return err
+			}
+			builder = schema1.NewConfigManifestBuilder(p.repo.Blobs(ctx), p.config.TrustKey, manifestRef, imgConfig)
+			manifest, err = manifestFromBuilder(ctx, builder, descriptors)
+			if err != nil {
+				return err
+			}
+			if _, err = manSvc.Put(ctx, manifest, putOptions...); err != nil {
+				return err
+			}
 		}
 	}
 