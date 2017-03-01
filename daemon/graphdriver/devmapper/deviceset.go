7b110d42bb1b65b949702d5f5faa1ff245b105a7
Lei Jitang <leijitang@huawei.com>
2017-03-01 21:58:14 +0800

    devicemapper: fix suspend removed device
    when doing devices.cancelDeferredRemoval, the device could have been removed
    and return ErrEnxio, but it continue to check if it is need to do suspend.
    doSuspend := devinfo != nil && devinfo.Exists != 0 uses a devinfo which is
    get before devices.cancelDeferredRemoval(baseInfo), it is outdate, the device
    has been removed and there is no need to do suspend. If do suspend it will return
    devicemapper: Error running deviceSuspend dm_task_run failed.
    
    Signed-off-by: Lei Jitang <leijitang@huawei.com>

--- daemon/graphdriver/devmapper/deviceset.go
+++ daemon/graphdriver/devmapper/deviceset.go
@@ -862,6 +862,7 @@ func (devices *DeviceSet) takeSnapshot(hash string, baseInfo *devInfo, size uint
 				if err != devicemapper.ErrEnxio {
 					return err
 				}
+				devinfo = nil
 			} else {
 				defer devices.deactivateDevice(baseInfo)
 			}
