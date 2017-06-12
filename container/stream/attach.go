19ea2d3b0225f99dede6ac686c0c05b41098bcf9
Antonio Murdaca <runcom@redhat.com>
2017-06-13 01:34:20 +0200

    container/stream/attach: use pools.Copy
    The use of pools.Copy avoids io.Copy's internal buffer allocation.
    This commit replaces io.Copy with pools.Copy to avoid the allocation of
    buffers in io.Copy.
    
    Signed-off-by: Cristian Staretu <cristian.staretu@gmail.com>
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- container/stream/attach.go
+++ container/stream/attach.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/docker/docker/pkg/pools"
 	"github.com/docker/docker/pkg/promise"
 )
 
@@ -92,7 +93,7 @@ func (c *Config) CopyStreams(ctx context.Context, cfg *AttachConfig) chan error 
 		if cfg.TTY {
 			_, err = copyEscapable(cfg.CStdin, cfg.Stdin, cfg.DetachKeys)
 		} else {
-			_, err = io.Copy(cfg.CStdin, cfg.Stdin)
+			_, err = pools.Copy(cfg.CStdin, cfg.Stdin)
 		}
 		if err == io.ErrClosedPipe {
 			err = nil
@@ -122,7 +123,7 @@ func (c *Config) CopyStreams(ctx context.Context, cfg *AttachConfig) chan error 
 		}
 
 		logrus.Debugf("attach: %s: begin", name)
-		_, err := io.Copy(stream, streamPipe)
+		_, err := pools.Copy(stream, streamPipe)
 		if err == io.ErrClosedPipe {
 			err = nil
 		}
@@ -231,5 +232,5 @@ func copyEscapable(dst io.Writer, src io.ReadCloser, keys []byte) (written int64
 	pr := &ttyProxy{escapeKeys: keys, r: src}
 	defer src.Close()
 
-	return io.Copy(dst, pr)
+	return pools.Copy(dst, pr)
 }
